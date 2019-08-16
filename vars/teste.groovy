747e6f707de10127f38fa76c84fa58fe515e99a3
ROGER LOZADA <roger_ti2008@yahoo.com.br>
2019-08-16 14:17:55 -0300

    Update teste.groovy

--- vars/teste.groovy
+++ vars/teste.groovy
@@ -3,6 +3,11 @@ def call2(String name = 'human') {
   echo "Hello5, ${name}."
 }
 
+def call3(String name = 'human') {
+  echo "Hello5, ${name}."
+}
+
+
 class DiaDeSemana {
   DiaDeSemana(String _name){
     this.name = _name;
